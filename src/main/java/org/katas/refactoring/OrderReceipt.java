10c9297c03afd915a0782cd3ebc5cf0582de85b8
zllisme <1973842936@qq.com>
2019-07-15 19:40:37 +0800

    refactor: change o vriable'name to order

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -7,10 +7,10 @@ package org.katas.refactoring;
  * total sales tax) and prints it.
  */
 public class OrderReceipt {
-    private Order o;
+    private Order order;
 
     public OrderReceipt(Order o) {
-        this.o = o;
+        this.order = o;
     }
 
     public String printReceipt() {
@@ -20,15 +20,13 @@ public class OrderReceipt {
         output.append("======Printing Orders======\n");
 
         // print date, bill no, customer name
-//        output.append("Date - " + order.getDate();
-        output.append(o.getCustomerName());
-        output.append(o.getCustomerAddress());
-//        output.append(order.getCustomerLoyaltyNumber());
+        output.append(order.getCustomerName());
+        output.append(order.getCustomerAddress());
 
         // prints lineItems
         double totSalesTx = 0d;
         double tot = 0d;
-        for (LineItem lineItem : o.getLineItems()) {
+        for (LineItem lineItem : order.getLineItems()) {
             output.append(lineItem.getDescription());
             output.append('\t');
             output.append(lineItem.getPrice());
